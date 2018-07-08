8420ef88ff1f884f7f19c23c1693952309026ba8
Noobab <tomowen84@yahoo.co.uk>
2018-07-08 21:55:17 +0100

    Created empty web api project

---
+++ CheckoutApi/Controllers/OrdersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CheckoutApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Hello", "World" };
+        }
+    }
+}
